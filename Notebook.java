2b1e157bc18c7a8b87c3e325b18619fa0c1ca7e0
Mikhail <un.less.jk@gmail.com>
2023-06-30 09:46:09 +0300

    made task1 Homework6

--- Notebook.java
+++ Notebook.java
@@ -8,12 +8,10 @@ import java.util.Map;
 public class Notebook {
 
     private String name;
-
     private int amountRAM;
     private String operatingSystem;
     private int price;
     private String model;
-
     public Notebook(String name, int amountRAM, String operatingSystem, int price, String model) {
         this.name = name;
         this.amountRAM = amountRAM;
@@ -21,20 +19,16 @@ public class Notebook {
         this.price = price;
         this.model = model;
     }
-
     public boolean validateObject(){
         return true;
     }
-
     public static List<String> propertiesForFilter(){
         List<String> list = new ArrayList<>();
         list.add("amountRAM");
         list.add("operatingSystem");
         list.add("price");
         list.add("model");
-
         return list;
-
     }
 
     @Override
@@ -45,45 +39,25 @@ public class Notebook {
                 ", цена: " + price +
                 ", модель: " + model;
     }
-
-    public String getName() {
-        return name;
+    public String getName() {return name;
     }
-
-    public void setName(String name) {
-        this.name = name;
+    public void setName(String name) {this.name = name;
     }
-
-    public int getAmountRAM() {
-        return amountRAM;
+    public int getAmountRAM() {return amountRAM;
     }
-
-    public void setAmountRAM(int amountRAM) {
-        this.amountRAM = amountRAM;
+    public void setAmountRAM(int amountRAM) {this.amountRAM = amountRAM;
     }
-
-    public String getOperatingSystem() {
-        return operatingSystem;
+    public String getOperatingSystem() {return operatingSystem;
     }
-
-    public void setOperatingSystem(String operatingSystem) {
-        this.operatingSystem = operatingSystem;
+    public void setOperatingSystem(String operatingSystem) {this.operatingSystem = operatingSystem;
     }
-
-    public int getPrice() {
-        return price;
+    public int getPrice() {return price;
     }
-
-    public void setPrice(int price) {
-        this.price = price;
+    public void setPrice(int price) {this.price = price;
     }
-
-    public String getModel() {
-        return model;
+    public String getModel() {return model;
     }
-
-    public void setModel(String model) {
-        this.model = model;
+    public void setModel(String model) {this.model = model;
     }
 }
 
